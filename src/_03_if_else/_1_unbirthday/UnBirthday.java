c0986b1f02a9136c4969e5864a6343522632d575
root <root@73b5302b1171>
2020-04-16 23:57:24 +0000

    Secret Message Box

---
+++ src/_03_if_else/_1_unbirthday/UnBirthday.java
@@ -0,0 +1,13 @@
+package _03_if_else._1_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class UnBirthday {
+public static void main(String[] args) {
+	String input = JOptionPane.showInputDialog("When is your birthday? No year");
+			if(input.equals("04/16")) {
+JOptionPane.showMessageDialog(null, "HAPPY BIRTHDAY");
+System.out.println("HAPPYBIRTHDay")
+			}			
+			else {JOptionPane.showConfirmDialog(null, "HAPPY UNBIRTHDAY");		}	}
+}
--- src/_03_if_else/_3_secret_message_box/SecretMessageBox.java
+++ src/_03_if_else/_3_secret_message_box/SecretMessageBox.java
@@ -17,16 +17,22 @@ import javax.swing.JOptionPane;
 public class SecretMessageBox {
 
 	// 0. Make a main method and put steps 1-5 inside it
+	public static void main(String[] args) {
+		
 	
 	// 1. Set a password in a String variable
-	
+	String Password = "12344321";
 	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
-
+String SecretMessage = JOptionPane.showInputDialog("What is your Secret Message?");
 	// 3. Now use a pop-up to tell the NEXT user that they can only see the secret message 
 	//    if they can guess the passcode
-
+JOptionPane.showMessageDialog(null, "You can only see the message if you know the password");
 	// 4. If their guess matches the password, show them the secret message
-
+String Attempt = JOptionPane.showInputDialog("What is the password?");
 	// 5. If the password does not match, pop-up "INTRUDER!!"
-
+		if(Password.equals (Attempt)) {
+			JOptionPane.showMessageDialog(null, message);
+			
+		}
+	}
 }
